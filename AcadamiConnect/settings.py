c47ac2b96f38d8ed282fe1967f5a552a86772743
khushburajak <khushburajak17@gmail.com>
2023-12-12 17:30:21 +0545

    Changes Database to postgreSql

--- AcadamiConnect/settings.py
+++ AcadamiConnect/settings.py
@@ -32,13 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'course'
+    'course',
+    'ckeditor'
+
 ]
 
 MIDDLEWARE = [
@@ -75,12 +78,27 @@ WSGI_APPLICATION = 'AcadamiConnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# Configure Django settings for PostgreSQL
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AcadamiConnect',
+        'USER': 'postgres',
+        'PASSWORD': 'mko0',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
