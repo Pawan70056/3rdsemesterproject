f32467e50708a1aaf7eb34f89da897c10d2bcbc2
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2024-01-07 09:38:51 +0545

    content

--- course/migrations/0002_content.py
+++ course/migrations/0002_content.py
@@ -11,15 +11,4 @@ class Migration(migrations.Migration):
         ('course', '0001_initial'),
     ]
 
-    operations = [
-        migrations.CreateModel(
-            name='Content',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('content_type', models.CharField(choices=[('text', 'Text'), ('pdf', 'PDF')], default='text', max_length=4)),
-                ('text_content', ckeditor.fields.RichTextField(blank=True, null=True)),
-                ('pdf_file', models.FileField(blank=True, null=True, upload_to='pdf_contents/')),
-                ('chapter', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='contents', to='course.chapter')),
-            ],
-        ),
-    ]
+    
