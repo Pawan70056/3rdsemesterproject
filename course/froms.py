845d6bac01ad8c4a15470439317e4c3c126c4bca
khushburajak <khushburajak17@gmail.com>
2024-01-09 14:56:44 +0545

    feedback froms.py updated

--- course/froms.py
+++ course/froms.py
@@ -1,10 +1,16 @@
 # feedback_reviews/forms.py
 
 from django import forms
-from .models import Feedback
+from .models import Feedback, ImprovementArea
 
 class FeedbackForm(forms.ModelForm):
+    improvement_areas = forms.ModelMultipleChoiceField(
+        queryset=ImprovementArea.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
     class Meta:
         model = Feedback
-        fields = ['subject', 'message']  # Define the fields from the Feedback model
-        # You can add widgets, labels, or customize form field attributes here if needed
+        fields = ['name', 'email', 'age', 'employment_status', 'experience_level', 'course_rating',
+                  'materials_rating', 'recommend_course', 'improvement_areas', 'comments']
